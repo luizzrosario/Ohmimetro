6ece4f170705356ca5957f7da5d3b0a324c53d6a
Luiz Rosário <lfrasilva.cic@uesc.br>
2025-04-26 16:16:52 -0300

    chore: Initital commit

---
+++ Ohmimetro.c
@@ -0,0 +1,111 @@
+/*
+ * Por: Wilton Lacerda Silva
+ *    Ohmímetro utilizando o ADC da BitDogLab
+ *
+ * 
+ * Neste exemplo, utilizamos o ADC do RP2040 para medir a resistência de um resistor
+ * desconhecido, utilizando um divisor de tensão com dois resistores.
+ * O resistor conhecido é de 10k ohm e o desconhecido é o que queremos medir.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include "hardware/i2c.h"
+#include "lib/ssd1306.h"
+#include "lib/font.h"
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
+#define ADC_PIN 28 // GPIO para o voltímetro
+#define Botao_A 5  // GPIO para botão A
+
+int R_conhecido = 10000;   // Resistor de 10k ohm
+float R_x = 0.0;           // Resistor desconhecido
+float ADC_VREF = 3.31;     // Tensão de referência do ADC
+int ADC_RESOLUTION = 4095; // Resolução do ADC (12 bits)
+
+// Trecho para modo BOOTSEL com botão B
+#include "pico/bootrom.h"
+#define botaoB 6
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+  reset_usb_boot(0, 0);
+}
+
+int main()
+{
+  // Para ser utilizado o modo BOOTSEL com botão B
+  gpio_init(botaoB);
+  gpio_set_dir(botaoB, GPIO_IN);
+  gpio_pull_up(botaoB);
+  gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+  // Aqui termina o trecho para modo BOOTSEL com botão B
+
+  gpio_init(Botao_A);
+  gpio_set_dir(Botao_A, GPIO_IN);
+  gpio_pull_up(Botao_A);
+
+  // I2C Initialisation. Using it at 400Khz.
+  i2c_init(I2C_PORT, 400 * 1000);
+
+  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+  gpio_pull_up(I2C_SDA);                                        // Pull up the data line
+  gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
+  ssd1306_t ssd;                                                // Inicializa a estrutura do display
+  ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+  ssd1306_config(&ssd);                                         // Configura o display
+  ssd1306_send_data(&ssd);                                      // Envia os dados para o display
+
+  // Limpa o display. O display inicia com todos os pixels apagados.
+  ssd1306_fill(&ssd, false);
+  ssd1306_send_data(&ssd);
+
+  adc_init();
+  adc_gpio_init(ADC_PIN); // GPIO 28 como entrada analógica
+
+  float tensao;
+  char str_x[5]; // Buffer para armazenar a string
+  char str_y[5]; // Buffer para armazenar a string
+
+  bool cor = true;
+  while (true)
+  {
+    adc_select_input(2); // Seleciona o ADC para eixo X. O pino 28 como entrada analógica
+
+    float soma = 0.0f;
+    for (int i = 0; i < 500; i++)
+    {
+      soma += adc_read();
+      sleep_ms(1);
+    }
+    float media = soma / 500.0f;
+
+      // Fórmula simplificada: R_x = R_conhecido * ADC_encontrado /(ADC_RESOLUTION - adc_encontrado)
+      R_x = (R_conhecido * media) / (ADC_RESOLUTION - media);
+
+    sprintf(str_x, "%1.0f", media); // Converte o inteiro em string
+    sprintf(str_y, "%1.0f", R_x);   // Converte o float em string
+
+    // cor = !cor;
+    //  Atualiza o conteúdo do display com animações
+    ssd1306_fill(&ssd, !cor);                          // Limpa o display
+    ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);      // Desenha um retângulo
+    ssd1306_line(&ssd, 3, 25, 123, 25, cor);           // Desenha uma linha
+    ssd1306_line(&ssd, 3, 37, 123, 37, cor);           // Desenha uma linha
+    ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6); // Desenha uma string
+    ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 16);  // Desenha uma string
+    ssd1306_draw_string(&ssd, "  Ohmimetro", 10, 28);  // Desenha uma string
+    ssd1306_draw_string(&ssd, "ADC", 13, 41);          // Desenha uma string
+    ssd1306_draw_string(&ssd, "Resisten.", 50, 41);    // Desenha uma string
+    ssd1306_line(&ssd, 44, 37, 44, 60, cor);           // Desenha uma linha vertical
+    ssd1306_draw_string(&ssd, str_x, 8, 52);           // Desenha uma string
+    ssd1306_draw_string(&ssd, str_y, 59, 52);          // Desenha uma string
+    ssd1306_send_data(&ssd);                           // Atualiza o display
+    sleep_ms(700);
+  }
+}
